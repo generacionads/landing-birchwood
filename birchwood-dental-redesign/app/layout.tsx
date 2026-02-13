f58169da825cd4c29b13317c9a080e6b378f4a5e
mario <hola@generacionads.com>
2026-02-13 09:46:57 +0100

    tag manager

--- birchwood-dental-redesign/app/layout.tsx
+++ birchwood-dental-redesign/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const lato = Lato({
@@ -21,6 +22,21 @@ export default function RootLayout({
     return (
         <html lang="en-GB">
             <body className={`${lato.variable} font-sans bg-slate-50 text-slate-800 antialiased selection:bg-brand-200 selection:text-brand-900`}>
+                <Script id="gtm" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MMQQ9N9D');`}
+                </Script>
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MMQQ9N9D"
+                        height="0"
+                        width="0"
+                        style={{ display: "none", visibility: "hidden" }}
+                    />
+                </noscript>
                 {children}
             </body>
         </html>
